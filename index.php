41a47373d2c57e86687993488a11e05e9b6af886
ronaldsianypar <ronaldsianypar@gmail.com>
2021-01-29 10:39:03 +0700

    Calculator

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Belajar OOP</title>
+</head>
+<body>
+<form action="calc.php" method="POST">
+	<input type="text" name="num1">
+	<input type="text" name="num2">
+	<select name="cal">
+		<option value="add">+</option>
+		<option value="sub">-</option>
+		<option value="mul">*</option>
+	</select>
+	<button type="submit">Calculate</button>
+</form>
+</body>
+</html>
